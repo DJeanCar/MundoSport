9eaae16ab26b9a92c5095ed601f24cf9fa9ce930
DJeanCar <jean_carlos104@hotmail.com>
2017-09-04 02:34:14 -0500

    static

--- server/config/static.js
+++ server/config/static.js
@@ -3,6 +3,7 @@ import express from 'express';
 export default (app) => {
 
   if (process.env.NODE_ENV === 'prod') {
+    app.use(express.static(__dirname + '/../../client'));
     app.locals.STATIC_URL = 'https://s3-us-west-2.amazonaws.com/tarefastatic/static/';
   } else {
     app.locals.STATIC_URL = '/';
